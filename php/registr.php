8e8989a8850dac038193c9dee027ad69d499c0f5
Mel Pixel <melissapixell@gmail.com>
2024-05-13 13:56:35 +0300

    post

--- php/registr.php
+++ php/registr.php
@@ -1,8 +1,12 @@
 <div class="registr__form">
     <form action="index.php?page=registr" method="POST">
+        
         <div class="input__block"><input type="text" class="input__text" name="name" value="" placeholder="Имя"></div>
+
         <div class="input__block"><input type="password" class="input__text" name="password" value="" placeholder="Пароль"></div>
+
         <div class="input__block"><input type="password" class="input__text" name="password_2" value="" placeholder="Пароль повторно"></div>
+
         <div class="input__block"><input type="submit" class="input__button" value="Отправить"></div>
     </form>
 </div>
--- php/setting.php
+++ php/setting.php
@@ -19,4 +19,15 @@
     $sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), password varchar(30), password_2 varchar(30));";
     // выполняем SQL-выражение
     $conn->exec($sql);
+
+    if (isset($_POST["name"]) && isset($_POST["password"]) && isset($_POST["password_2"])) {
+
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+        $password_2 = $_POST["password_2"];
+
+        $sql = "INSERT INTO users (name, password, password_2) VALUES ($name, $password, $password_2)";
+        $conn->exec($sql);
+        header('Location: index.php', true, 303);
+    }
 ?>
