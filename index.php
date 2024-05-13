c7642cb98a16fba95fd285e25e54f92a2e959bbf
Mel Pixel <melissapixell@gmail.com>
2024-05-13 10:54:00 +0300

    load

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 <?php
     include 'php\setting.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<<<<<<< HEAD
 =======
 <!DOCTYPE html>
 <html lang="ru-RU">
