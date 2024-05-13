0e60ac42dd3754f9a8afb05c6056ffb44c852897
Mel Pixel <melissapixell@gmail.com>
2024-05-13 16:36:04 +0300

    checPassword

--- php/setting.php
+++ php/setting.php
@@ -5,6 +5,18 @@
 
 // Все получилось.
 
+function is_password(string $password): bool
+{
+    if (
+        mb_strlen($password) >= 8
+        and preg_match('/[A-Z]/', $password)
+        and preg_match('/[a-z]/', $password)
+        and preg_match('/[0-9]/', $password)
+    ) {
+        return true;  // Passed all tests
+    }
+    return false;     // Invalid
+}
     // подключаемся к серверу
     $conn = new PDO("mysql:host=localhost", "root", "");
      
@@ -20,21 +32,28 @@
     // выполняем SQL-выражение
     $conn->exec($sql);
 
-    if (isset($_POST["name"]) && isset($_POST["password"]) && isset($_POST["password_2"])) {
+    if (isset($_POST["name"]) AND isset($_POST["password"]) AND isset($_POST["password_2"])) {
         $conn = new PDO("mysql:host=localhost;dbname=example1", "root", "");
+
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+        $password_2 = $_POST["password_2"];
+
+        $valid    = is_password($password);
         
-        // $name = $_POST["name"];
-        // $password = $_POST["password"];
-        // $password_2 = $_POST["password_2"];
+        if ($valid == true AND $password === $password_2) {
+            $sql = "INSERT INTO users (name, password, password_2) VALUES (:name, :password, :password_2)";
 
-        $sql = "INSERT INTO users (name, password, password_2) VALUES (:name, :password, :password_2)";
+            $stmt = $conn->prepare($sql);
 
-        $stmt = $conn->prepare($sql);
-
-        $stmt->bindValue(":name", $_POST["name"]);
-        $stmt->bindValue(":password", $_POST["password"]);
-        $stmt->bindValue(":password_2", $_POST["password_2"]);
-        $stmt->execute();
-        header('Location: index.php', true, 303);
+            $stmt->bindValue(":name", $name);
+            $stmt->bindValue(":password", $password);
+            $stmt->bindValue(":password_2", $password_2);
+            $stmt->execute();
+            header('Location: index.php', true, 303);
+        }
+         else {
+            echo 'Password not strong enough or password values ​​are different.';
+        }
     }
 ?>
