0be3c60efbc589541bba66b0c127293d4db3b084
Mel Pixel <melissapixell@gmail.com>
2024-05-13 13:24:10 +0300

    tableUsers

--- php/setting.php
+++ php/setting.php
@@ -12,4 +12,11 @@
     $sql = "CREATE DATABASE IF NOT EXISTS example1";
     // выполняем SQL-выражение
     $conn->exec($sql);
+
+    $conn = new PDO("mysql:host=localhost;dbname=example1", "root", "");
+
+    // SQL-выражение для создания таблицы
+    $sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), password varchar(30), password_2 varchar(30));";
+    // выполняем SQL-выражение
+    $conn->exec($sql);
 ?>
