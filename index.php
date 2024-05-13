65a271423345af6ad9386c3f365b37840dcb373f
pullock <pullock@yandex.ru>
2024-05-13 10:26:12 +0300

    projct

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="">
+        <meta name="discription" content="Тестовое задание">
+        <meta name="author" content="Петр Ласточкин">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/style.css?24">
+        <title>PDO, классы</title>
+    </head>
+    <body>
+        <div class="main__block">
+            <?php include 'php/registr.php'; ?>
+        </div>
+    </body>
+    <script src="js/index.js"></script>
+</html>
